f881271270be365d7f375a4366f2220bf2802485
Zahran Yahia Khan <114562123+zahran001@users.noreply.github.com>
2025-06-28 14:08:17 -0400

    initial setup

---
+++ DemoLibrary/DataAccess/DemoDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using DemoLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary.DataAccess
+{
+    public class DemoDataAccess : IDataAccess
+    {
+        private List<PersonModel> people = new();
+
+        public DemoDataAccess()
+        {
+            people.Add(new PersonModel { Id = 1, FirstName = "John", LastName = "Doe" });
+            people.Add(new PersonModel { Id = 2, FirstName = "Tim", LastName = "Carry" });
+        }
+
+        // 2 methods
+        public List<PersonModel> GetPeople()
+        {
+            return people;
+        }
+
+        public PersonModel InsertPerson(string firstName, string lastName)
+        {
+            PersonModel p = new() { FirstName = firstName, LastName = lastName };
+            p.Id = people.Max(x => x.Id) + 1;
+            people.Add(p);
+            return p;
+        }
+    }
+}
